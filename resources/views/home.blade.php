c6bc7611729a8544ee063b770f69f2c556816353
Sibtain <sibtainhaider369@gmail.com>
2022-01-22 17:34:59 +0500

    #1-dashboard-module

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                        <a href="/dashboard">Dashboard</a>
                 </div>
             </div>
         </div>
