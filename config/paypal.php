19c4c9afbfe12dc27a24e9cfe732de26aba76bd1
loopers1199@gmail.com <loopers1199@gmail.com>
2022-02-02 23:17:48 +0400

    #19-paypent added paypal and stripe

---
+++ config/paypal.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PayPal Setting & API Credentials
+ * Created by Raza Mehdi <srmk@outlook.com>.
+ */
+
+return [
+    'mode'    => env('PAYPAL_MODE', 'sandbox'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+    'sandbox' => [
+        'client_id'         => 'ARSF761nLnocqXAS7DbR-v0brxFfEo9r4y3-pIx6emNm69_Ao4OQuooZaAH3R5ELU87sIq-aark8K3E1',
+        'client_secret'     => 'EG4sNZ1bdY9bT2YkhNxULSytwd5hKUdQxL1vdDedo-AWV5gJyHcDJn-qfI2r2hG12Bwy1ZDWOR-LLec7',
+        'app_id'            => 'APP-80W284485P519543T',
+    ],
+    'live' => [
+        'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', ''),
+        'client_secret'     => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
+        'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
+    ],
+
+    'payment_action' => env('PAYPAL_PAYMENT_ACTION', 'Sale'), // Can only be 'Sale', 'Authorization' or 'Order'
+    'currency'       => env('PAYPAL_CURRENCY', 'USD'),
+    'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
+    'locale'         => env('PAYPAL_LOCALE', 'en_US'), // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+    'validate_ssl'   => env('PAYPAL_VALIDATE_SSL', true), // Validate SSL when creating api client.
+];
