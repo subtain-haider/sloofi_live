ecb2420d7775dcfbe0f0b7a6d879e9b44748853b
Sibtain <sibtainhaider369@gmail.com>
2022-02-03 22:12:49 +0500

    done

--- Modules/Frontend/Resources/views/index.blade.php
+++ Modules/Frontend/Resources/views/index.blade.php
@@ -26,9 +26,6 @@
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 6"></button>
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
@@ -38,17 +35,9 @@
                 <img src="{{url('/frontend')}}/images/slider2.jpg" class="d-block w-100" alt="">
               </div>
               <div class="carousel-item">
-                <img src="{{url('/frontend')}}/images/slider.jpg" class="d-block w-100" alt="">
+                <img src="{{url('/frontend')}}/images/slider3.jpg" class="d-block w-100" alt="">
               </div>
-              <div class="carousel-item">
-                <img src="{{url('/frontend')}}/images/slider2.jpg" class="d-block w-100" alt="">
-              </div>
-              <div class="carousel-item">
-                <img src="{{url('/frontend')}}/images/slider.jpg" class="d-block w-100" alt="">
-              </div>
-              <div class="carousel-item">
-                <img src="{{url('/frontend')}}/images/slider2.jpg" class="d-block w-100" alt="">
-              </div>
+
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
