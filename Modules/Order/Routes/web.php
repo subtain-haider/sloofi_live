e9a762ca9bb3293fa4888ebc9f89c2bd2cda75e5
loopers1199@gmail.com <loopers1199@gmail.com>
2022-02-05 13:26:10 +0400

    #21-order

---
+++ Modules/Order/Routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('order')->group(function() {
+    Route::controller(OrderController::class)->group(function () {
+        Route::get('orders/all', 'index')->name('order.all');
+        Route::get('orders/sloofi', 'sloofiOrders')->name('order.sloofi');
+        Route::get('orders/internal', 'internalOrders')->name('order.internal');
+        Route::get('orders/external', 'externalOrders')->name('order.external');
+    });
+});
