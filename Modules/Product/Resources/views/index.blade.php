e616106599366e5cad76268be733de288dac102f
loopers1199@gmail.com <loopers1199@gmail.com>
2022-02-11 22:08:38 +0400

    point 2,5

--- Modules/Product/Resources/views/index.blade.php
+++ Modules/Product/Resources/views/index.blade.php
@@ -14,7 +14,7 @@
                                 <div class="nk-block-head-content">
                                     <h4 class="nk-block-title">Products</h4>
                                     <div class="nk-block-des">
-                                       
+
                                     </div>
                                 </div>
                             </div>
