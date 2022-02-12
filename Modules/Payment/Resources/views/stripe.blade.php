9b7176f969582e1fdec1847376d9df6f0404aa14
loopers1199@gmail.com <loopers1199@gmail.com>
2022-02-12 12:57:11 +0400

    mobile sign up

--- Modules/Payment/Resources/views/stripe.blade.php
+++ Modules/Payment/Resources/views/stripe.blade.php
@@ -1,97 +1,5 @@
-@extends('frontend::layouts.master')
-    @section('css')
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-@endsection
-@section('content')
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3" style="margin-top: 50px">
-                <div class="panel panel-default credit-card-box">
-                    <div class="panel-heading display-table" >
-                        <div class="row display-tr" >
-                            <h3 class="panel-title display-td" >Payment Details</h3>
-{{--                            <div class="display-td" >--}}
-{{--                                <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">--}}
-{{--                            </div>--}}
-                        </div>
-                    </div>
-                    <div class="panel-body">
-
-                        @if (Session::has('success'))
-                            <div class="alert alert-success text-center">
-                                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                                <p>{{ Session::get('success') }}</p>
-                            </div>
-                        @endif
-
-                        <form role="form" action="{{ route('stripe.post') }}" method="post" class="require-validation"
-                              data-cc-on-file="false"
-                              data-stripe-publishable-key="{{ env('STRIPE_KEY') }}"
-                              id="payment-form">
-                            @csrf
-                            <input type="hidden" name="order_id" value="{{$order->id}}">
-                            <div class='form-row row'>
-                                <div class='col-xs-12 form-group required'>
-                                    <label class='control-label'>Name on Card</label> <input
-                                        class='form-control' size='4' type='text'>
-                                </div>
-                            </div>
-
-                            <div class='form-row row'>
-                                <div class='col-xs-12 form-group card required'>
-                                    <label class='control-label'>Card Number</label> <input
-                                        autocomplete='off' class='form-control card-number' size='20'
-                                        type='text'>
-                                </div>
-                            </div>
-
-                            <div class='form-row row'>
-                                <div class='col-xs-12 col-md-4 form-group cvc required'>
-                                    <label class='control-label'>CVC</label> <input autocomplete='off'
-                                                                                    class='form-control card-cvc' placeholder='ex. 311' size='4'
-                                                                                    type='text'>
-                                </div>
-                                <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                    <label class='control-label'>Expiration Month</label> <input
-                                        class='form-control card-expiry-month' placeholder='MM' size='2'
-                                        type='text'>
-                                </div>
-                                <div class='col-xs-12 col-md-4 form-group expiration required'>
-                                    <label class='control-label'>Expiration Year</label> <input
-                                        class='form-control card-expiry-year' placeholder='YYYY' size='4'
-                                        type='text'>
-                                </div>
-                            </div>
-
-                            <div class='form-row row'>
-                                <div class='col-md-12 error form-group hide'>
-                                    <div class='alert-danger alert'>Please correct the errors and try
-                                        again.</div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    <button class="btn btn-primary btn-lg btn-block" type="submit" style="background: #d93644">Pay Now (${{$order->total}})</button>
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-@endsection
-
-
-
-
-@section('js')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 
 <script type="text/javascript">
@@ -148,4 +56,4 @@
 
     });
 </script>
-@endsection
+
